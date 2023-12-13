3bf6f79e47aaade4af83893ad1786620ee53a114
Luis Martinez <luis.martinez@newfold.com>
2023-12-13 12:57:44 -0400

    update

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://servicios.mirex.gob.do/proxy/api/padron"
 
 export const PROFILE_IMAGES = {
-    M: '../images/man.png',
-    F: '../images/woman.png'
+    M: 'images/man.png',
+    F: 'images/woman.png'
 }
